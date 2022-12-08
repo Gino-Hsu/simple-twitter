2e27d32b5cf482e34b3e506472530327d3eed827
jeff0518 <cycivs07@icloud.com>
2022-12-09 01:41:49 +0800

    refactor: component:  SideBar Using css variables, and changing some errors

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import './App.scss'
 // import Regist from './pages/Regist'
-import Login from './pages/Login'
+// import Login from './pages/Login'
+import SideBar from './components/SideBar/SideBar'
 
 function App() {
   return (
     <div className="App">
       {/* <Regist /> */}
-      <Login />
+      {/* <Login /> */}
+      <SideBar />
     </div>
   )
 }
