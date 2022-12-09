84efcefb49d4861d552b790ee188d3314a52927d
jeff0518 <cycivs07@icloud.com>
2022-12-09 11:40:25 +0800

    feat: add component : PopularUser Add PopularUser component and complete RWD, modify the layout bug

--- src/UIComponents/PopularUserItem.jsx
+++ src/UIComponents/PopularUserItem.jsx
@@ -2,19 +2,20 @@ import React from 'react'
 import ButtonUI from './ButtonUI'
 import style from './PopularUserItem.module.scss'
 
-export default function PopularUserItem() {
+export default function PopularUserItem({ btnStyle, text, container }) {
   return (
     <div className={style.item}>
       <div className={style.user__avatar}>
         <img src="" alt="logo" />
       </div>
       <div className={style.user__info}>
-        <p className={style.user__info__name}>Pizza Hut</p>
+        <p className={style.user__info__name}>PizzaHut1</p>
         <p className={style.user__info__account}>@pizzahut</p>
       </div>
       <div className={style.btn__follow}>
-        <ButtonUI btnStyle="btn__popularUser__following" text="正在跟隨" />
-        {/* <ButtonUI btnStyle="btn__popularUser__unFollow" text="跟隨" /> */}
+        <div className={style[container]}>
+          <ButtonUI btnStyle={btnStyle} text={text} />
+        </div>
       </div>
     </div>
   )
