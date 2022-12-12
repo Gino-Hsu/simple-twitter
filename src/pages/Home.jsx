15747b0d935007ce0d45612e2fa7d787fc763da1
jeff0518 <cycivs07@icloud.com>
2022-12-12 15:09:32 +0800

    feat: Complete routing settings

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,8 +2,6 @@ import React from 'react'
 import MainTweet from '../components/mainTweet/MainTweet'
 import style from './Home.module.scss'
 
-import Reply from '../pages/Reply'
-
 export default function Home() {
   return (
       <div className={style.main__screen}>
