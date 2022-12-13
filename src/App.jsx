c7604fcf7c9a8066b4e0c75a610a8bf4aebb6862
GinoHsu <ginohsu16@gmail.com>
2022-12-13 23:27:24 +0800

    config: fix config

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Layout from './pages/layout/Layout'
 import Login from './pages/Login'
 import Regist from './pages/Regist'
+import Setting from './pages/Setting'
 import AdminLogin from './pages/AdminLogin'
 import AdminTweets from './pages/AdminTweets'
 import AdminUsers from './pages/AdminUsers'
@@ -27,8 +28,7 @@ function App() {
           <Route path="/setting" element={<Setting />}></Route>
           <Route path="/admin" element={<AdminLogin />}></Route>
           <Route path="/admin/tweets" element={<AdminTweets />}></Route>
-          <Route path="/admin" element={<AdminLogin />}></Route>
-          <Route path="/setting" element={<Setting />}></Route>
+          <Route path="/admin/Users" element={<AdminUsers />}></Route>
           <Route path="/alphitter" element={<Layout />}>
             <Route path="home" element={<Home />}></Route>
             <Route path="reply" element={<Reply />}></Route>
@@ -46,16 +46,7 @@ function App() {
             <Route path="user/other/tweet" element={<OtherUserTweet />}></Route>
             <Route path="user/other/reply" element={<OtherUserReply />}></Route>
             <Route path="user/other/like" element={<OtherUserLike />}></Route>
-            {/* 
-            <Route path="user/self/follower" element={<Follower />}></Route>
-            <Route path="user/self/following" element={<Following />}></Route>
-            <Route path="/admin/Users" element={<AdminUsers />}></Route>
-            <Route path="/彈跳視窗" element={<TweetModal />}></Route>
-            <Route path="/彈跳視窗" element={<ReplyModal />}></Route>
-            <Route path="/彈跳視窗" element={<EditModal />}></Route> */}
           </Route>
-          <Route path="/admin/tweets" element={<AdminTweets />}></Route>
-          <Route path="/admin/Users" element={<AdminUsers />}></Route>
         </Routes>
       </BrowserRouter>
     </div>
@@ -63,3 +54,7 @@ function App() {
 }
 
 export default App
+
+///<Route path="/彈跳視窗" element={<TweetModal />}></Route>
+///<Route path="/彈跳視窗" element={<ReplyModal />}></Route>
+///<Route path="/彈跳視窗" element={<EditModal />}></Route>
