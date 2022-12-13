52e40c8fc4f8631bb296d58ac99499b8669fba0e
GinoHsu <ginohsu16@gmail.com>
2022-12-13 17:13:58 +0800

    feat: add OtherUserLike page

---
+++ src/pages/OtherUserLike.jsx
@@ -0,0 +1,88 @@
+import React from 'react'
+import OtherUser from '../components/otherUser/OtherUser'
+import TweetListItem from '../UIComponents/listItems/TweetListItem'
+
+import style from './OtherUserLike.module.scss'
+
+import cover from '../public/default_background@2x.png'
+import avatar from '../public/seed/81803399afee0c76ba618049dfdf2441.jpg'
+
+export default function OtherUserLike() {
+  return (
+    <div className={style.userLike__container}>
+      <OtherUser
+        coverImg={cover}
+        name="Gino"
+        account="gino"
+        avatarImg={avatar}
+        description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus rerum in quibusdam possimus atque velit, a adipisci perferendis, porro et enim ipsum dignissimos laboriosam veritatis quaerat tempore saepe doloribus laborum!"
+        followerCount="34"
+        followingCount="59"
+      >
+        <TweetListItem
+          tweet="Lorem ipsum dolor sit amet consectetur adipisicing elit."
+          userAvatar={avatar}
+          account="gino"
+          userName="Gino"
+          time="3 小時"
+          twitterReply="13"
+          twitterLike="76"
+        />
+        <TweetListItem
+          tweet="Lorem ipsum dolor sit amet consectetur adipisicing elit."
+          userAvatar={avatar}
+          account="gino"
+          userName="Gino"
+          time="3 小時"
+          twitterReply="13"
+          twitterLike="76"
+        />
+        <TweetListItem
+          tweet="Lorem ipsum dolor sit amet consectetur adipisicing elit."
+          userAvatar={avatar}
+          account="gino"
+          userName="Gino"
+          time="3 小時"
+          twitterReply="13"
+          twitterLike="76"
+        />
+        <TweetListItem
+          tweet="Lorem ipsum dolor sit amet consectetur adipisicing elit."
+          userAvatar={avatar}
+          account="gino"
+          userName="Gino"
+          time="3 小時"
+          twitterReply="13"
+          twitterLike="76"
+        />
+        <TweetListItem
+          tweet="Lorem ipsum dolor sit amet consectetur adipisicing elit."
+          userAvatar={avatar}
+          account="gino"
+          userName="Gino"
+          time="3 小時"
+          twitterReply="13"
+          twitterLike="76"
+        />
+        <TweetListItem
+          tweet="Lorem ipsum dolor sit amet consectetur adipisicing elit."
+          userAvatar={avatar}
+          account="gino"
+          userName="Gino"
+          time="3 小時"
+          twitterReply="13"
+          twitterLike="76"
+        />
+        <TweetListItem
+          tweet="Lorem ipsum dolor sit amet consectetur adipisicing elit."
+          userAvatar={avatar}
+          account="gino"
+          userName="Gino"
+          time="3 小時"
+          twitterReply="13"
+          twitterLike="76"
+        />
+      </OtherUser>
+    </div>
+  )
+}
