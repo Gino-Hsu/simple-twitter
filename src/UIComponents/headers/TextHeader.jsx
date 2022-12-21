bde9f47c68def669125e751f7d731435b4418f01
jeff0518 <cycivs07@icloud.com>
2022-12-21 21:13:37 +0800

    feat:back to previous page

--- src/UIComponents/headers/TextHeader.jsx
+++ src/UIComponents/headers/TextHeader.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 import style from './TextHeader.module.scss'
 
 export default function HeaderText() {
+  const navigate = useNavigate()
   return (
     <div className={style.header__container}>
       <div className={style.header__return}>
         <div className={style.icon}>
-          <div className={style.icon__container}>
+          <div onClick={() => navigate(-1)} className={style.icon__container}>
             <img className={style.img} src="" alt="" />
           </div>
         </div>
