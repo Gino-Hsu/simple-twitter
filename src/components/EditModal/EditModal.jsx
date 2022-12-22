031848830c735cd96ea2afcce99fa52fc08e729f
GinoHsu <ginohsu16@gmail.com>
2022-12-22 19:22:40 +0800

    fix: putUserEdit get token

--- src/components/EditModal/EditModal.jsx
+++ src/components/EditModal/EditModal.jsx
@@ -132,7 +132,7 @@ export default function EditModal({ handleHideModel }) {
       setCover(data.cover)
       setAvatar(data.avatar)
       setName(data.name)
-      setIntroduction(data.introduction)
+      setIntroduction(data.introductio || "")
     })
   }, [rerender])
 
